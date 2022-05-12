d547f7b08f26f7d4493570136a2329eeb88e88b6
Shikhar2003 <104097134+Shikhar2003@users.noreply.github.com>
2022-05-12 23:57:11 +0530

    Add files via upload

---
+++ Output_Assignment 3.py
@@ -0,0 +1,50 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/Shikhar Saxena/Desktop/ssignment-3_Shikhar Saxena_21107023.py
+Enter the number to be converted to binary: 35
+0b100011
+Enter the expression 7--9++88*7-9/3
+629.0
+Enter the number n: 4
+Enter the number r: 7
+Enter the angle a: 30
+Enter the angle b: 60
+Enter the number x1: 2
+Enter the number x2: 6
+Enter the number y1: 8
+Enter the number y2: 5
+(3+4)(5) =  35
+n(n-1)/2 =  12.0
+4(pi)r^2 =  62.83185307179586
+sqrt(r(cosa)^2 + r(sinb)^2) =  0.9038369165419095
+y2-y1/x2-x1 =  -0.75
+In the range(5)
+0
+1
+2
+3
+4
+In the range(3, 10)
+3
+4
+5
+6
+7
+8
+9
+In the range(4 ,13, 3)
+4
+7
+10
+In the range(15, 5, -2)
+15
+13
+11
+9
+7
+In the range(5, 3)
+Enter the number of hydrogen atoms: 2
+Enter the number of carbon atoms: 3
+Enter the number of oxygen atoms: 4
+102.04774
