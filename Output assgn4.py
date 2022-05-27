fc824c3ea8fca3aed8ca52085405129b3b725fc8
Shikhar2003 <104097134+Shikhar2003@users.noreply.github.com>
2022-05-27 23:58:10 +0530

    Add files via upload

---
+++ Output assgn4.py
@@ -0,0 +1,45 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/Shikhar Saxena/AppData/Local/Programs/Python/Python310/input 4.py
+Grading system of a school!
+Enter your marks obtained( out of 100): 92
+Your Grade is: A
+
+Enter any Year:2019
+2019 is not an Leap Year
+
+It's a one to one digit multiplication Game
+Question- 1 : 1 * 9
+Enter your result:9
+Right!
+Question- 2 : 5 * 5
+Enter your result:67
+Wrong! The correct answer is: 25
+Question- 3 : 8 * 3
+Enter your result:24
+Right!
+Question- 4 : 9 * 1
+Enter your result:7
+Wrong! The correct answer is: 9
+Question- 5 : 4 * 8
+Enter your result:76
+Wrong! The correct answer is: 32
+Question- 6 : 2 * 6
+Enter your result:12
+Right!
+Question- 7 : 1 * 1
+Enter your result:2
+Wrong! The correct answer is: 1
+Question- 8 : 9 * 6
+Enter your result:54
+Right!
+Question- 9 : 9 * 9
+Enter your result:81
+Right!
+Question- 10 : 6 * 7
+Enter your result:24
+Wrong! The correct answer is: 42
+
+
+There are total 177  candies in the bowl.
