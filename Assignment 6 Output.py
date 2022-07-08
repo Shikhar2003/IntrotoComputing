8c6479f36cf1c868a4ff851af0d1f5ff85b4d0c4
Shikhar2003 <104097134+Shikhar2003@users.noreply.github.com>
2022-07-08 22:34:13 +0530

    Add files via upload

---
+++ Assignment 6 Output.py
@@ -0,0 +1,36 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=========== RESTART: C:/Users/Shikhar Saxena/Desktop/Assignment 6.py ===========
+True
+False
+[1]
+[1, 1]
+[1, 2, 1]
+[1, 3, 3, 1]
+[1, 4, 6, 4, 1]
+[1, 5, 10, 10, 5, 1]
+True
+physics-chemistry-maths-biology-economics-accounts-psychology
+accounts-biology-chemistry-economics-maths-physics-psychology
+
+Student ID: 21107023
+Student Name: $ Shikhar Saxena
+
+Student ID: 21107023
+Student Name: $ Shikhar Saxena
+
+Student Name: $ Shikhar Saxena
+Student Class: $ XII
+True
+False
+True
+False
+
+Check whether the said classes are subclasses of the built-in object class or not.
+True
+True
+[[-10, 2, 8], [-7, -3, 10]]
+True
+False
+True
